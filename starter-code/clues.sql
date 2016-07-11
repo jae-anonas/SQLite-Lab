4d558296ab8369317c3ad2b1fe6b84d810639918
Jae Anonas <jae.anonas@gmail.com>
2016-07-11 17:29:31 -0400

    Jae Anonas - Ace Detective

--- starter-code/clues.sql
+++ starter-code/clues.sql
@@ -2,38 +2,39 @@
 -- traveling through Southern Europe. She's most likely traveling someplace where she won't be noticed,
 -- so find the least populated country in Southern Europe, and we'll start looking for her there.
 
-
-
+select code,name from country where region='Southern Europe' and population=(select min(population) from country where region='Southern Europe'); --VAT|Holy See (Vatican City State)
 -- Clue #2: Now that we're here, we have insight that Carmen was seen attending language classes in
 -- this country's officially recognized language. Check our databases and find out what language is
 -- spoken in this country, so we can call in a translator to work with you.
 
-
+select language from countrylanguage where countrycode='VAT'; --Italian
 
 -- Clue #3: We have new news on the classes Carmen attended – our gumshoes tell us she's moved on
 -- to a different country, a country where people speak only the language she was learning. Find out which
 --  nearby country speaks nothing but that language.
 
-
+select code,name from country where code=(select countrycode from countrylanguage where region="Southern Europe" and language='Italian' and percentage=100.0);
+--SMR|San Marino
 
 -- Clue #4: We're booking the first flight out – maybe we've actually got a chance to catch her this time.
  -- There are only two cities she could be flying to in the country. One is named the same as the country – that
  -- would be too obvious. We're following our gut on this one; find out what other city in that country she might
  --  be flying to.
 
-
+select id,name from city where countrycode='SMR' and name!='San Marino'; --Serravalle
 
 -- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different
 -- parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were
 -- headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
 
-
+select id,name,countrycode from city where name like 'Serr%'; --265|Serra|BRA
+select name from country where code='BRA'; --Brazil
 
 -- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards
  -- the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll
  -- follow right behind you!
 
-
+select name from city where id=(select capital from country where code='BRA'); -- Brasilia
 
 -- Clue #7: She knows we're on to her – her taxi dropped her off at the international airport, and she beat us to
  -- the boarding gates. We have one chance to catch her, we just have to know where she's heading and beat her to the
@@ -49,9 +50,10 @@
 -- So I'm off to add one to the population I find
 -- In a city of ninety-one thousand and now, eighty five.
 
+ select name from city where population=91084; --Santa Monica
 
 -- We're counting on you, gumshoe. Find out where she's headed, send us the info, and we'll be sure to meet her at the gates with bells on.
 
 
 
--- She's in ____________________________!
+-- She's in Santa Monica, California, USA!
